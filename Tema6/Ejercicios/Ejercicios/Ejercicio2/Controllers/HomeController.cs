c640d85a0a7269c72b154519312133c914f418f5
Juan Manuel Sanchez Moreno <jmmoreno@info2>
2023-10-19 11:51:35 +0200

    Terminado

---
+++ Tema6/Ejercicios/Ejercicios/Ejercicio2/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Ejercicio2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ejercicio2.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
