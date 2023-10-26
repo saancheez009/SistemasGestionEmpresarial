dcfc37b831fe6552a28943990c1136553cf7d4b5
Juan Manuel Sanchez Moreno <jmmoreno@info2>
2023-10-26 11:11:05 +0200

    por terminar

---
+++ Tema7/Ejercicio7/Ejercicio1/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Ejercicio1.Models;
+using Ejercicio1.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ejercicio1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            clsPersona persona = new clsPersona();
+            DateTime horaActual = DateTime.Now;
+
+            if (horaActual.Hour >= 5 && horaActual.Hour <= 12)
+            {
+                ViewData["Saludo"] = "Buenos días";
+
+            }
+            else if (horaActual.Hour > 12 && horaActual.Hour < 9)
+            {
+                ViewData["Saludo"] = "Buenas tardes";
+
+            }
+            else 
+            {
+                ViewData["Saludo"] = "Buenas noches";
+            }
+
+            ViewBag.HoraActual = horaActual;
+
+
+            persona.Nombre = "Juanma";
+            persona.Apellido = "Sanchez";
+            persona.Direccion = "¿Quieres raptarme?";
+
+            ViewBag.Nombre=persona.Nombre;
+            ViewBag.Apellido = persona.Apellido;
+            ViewBag.Direccion = persona.Direccion;
+
+
+            List<clsPersona> listaPers = new List<clsPersona>
+            {
+                new clsPersona("Juanma", "Sanchez"),
+                new clsPersona("Fernando", "Miguel"),
+                new clsPersona("Pablo", "Gonzalez"),
+                new clsPersona("Yeray", "Jimenez"),
+                new clsPersona("Matti", "Ditaranto"),
+            };
+            
+            ViewBag.listPers=listaPers;
+
+
+
+            return View();
+        }
+        
+
+    }
+}
---
+++ Tema7/Ejercicio7/Ejercicio1/Models/Entities/clsPersona.cs
@@ -0,0 +1,46 @@
+﻿namespace Ejercicio1.Models.Entities
+{
+    public class clsPersona
+    {
+        #region atributos
+        private string nombre;
+        private string apellido;
+        #endregion
+
+        #region constructores
+        public clsPersona()
+        {
+            this.nombre = "";
+            this.apellido = "";
+        }
+
+        public clsPersona(string nombre, string apellido)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+        #endregion
+
+        #region propiedades
+        public String Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public String Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+
+        public String Direccion { get; set; }
+
+        public String NombreCompleto
+        {
+            get { return $"{nombre} {apellido}"; }
+        }
+
+        #endregion
+    }
+}
